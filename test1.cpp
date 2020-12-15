733a7da5b21894be8bba58043950d3c699171fcb
Ting Lin <lintingsms@pku.edu.cn>
2020-12-15 13:44:46 +0800

    add .gitignore to filter executable file

--- test1.cpp
+++ test1.cpp
@@ -10,7 +10,11 @@ Vec gradf(Vec x){return x;}
 
 int main(){
     Problem<Vec>  p(L1_NORM);
+    p.f = f;
+    p.gradf = gradf;
+    p.mu = 1;
     Vec x(3);
-    x << 2,3,4;
+    x << -2,0.5,4;
     cout << p.proxh(x,1)<<endl;
+    cout << p.value(x) << endl;
 }
