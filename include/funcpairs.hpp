ccf9e6bd4c89ce4096a224d79584a6ab5309a34c
Ting Lin <lintingsms@pku.edu.cn>
2021-01-06 12:58:14 +0800

    Update funcpairs.hpp

--- include/funcpairs.hpp
+++ include/funcpairs.hpp
@@ -1,19 +1,43 @@
+// func pairs provide an interface for problem, such as  LS, norm ball of norm 
+// with easy construction method, the classes provide a simple but general/useful abstractions for proximal gradient methods.
+
+
+/* ------------------------------------
+        FUNCPAIRS.HPP
+-------------------------------------*/
+
 #ifndef __FUNCPAIRS_HPP__
 #define __FUNCPAIRS_HPP__
 #include <functional>
 #include <iostream>
 #include <eigen3/Eigen/Dense>
-// func pairs provide an interface for problem, such as  LS, norm ball of norm 
+
 using namespace std;
+
+/* type definition, you can change it if speicial requirement is needed. */
 typedef double Real;
 typedef Eigen::VectorXd Vec;
 typedef Eigen::MatrixXd Mat;
 
+
+/* ------------------------------------------------------------------------- 
+        CLASS DECLARATIONS
+------------------------------------------------------------------------- */
+
 template <typename T>
 class grad_pair{
+    /*
+    Class for smooth function. Containing 
+        - function<Real(T)> f : the value of function.
+        - function<T(T)> gradf: the gradient of function. 
+    */
     public:
+
+    /* construction functions */
     grad_pair(){}
     grad_pair(function<Real(T)> f, function<T(T)> gradf);
+
+    /* members */ 
 
     function<Real(T)> f;
     function<T(T)> gradf;
@@ -21,14 +45,28 @@ class grad_pair{
 
 template <typename T>
 class prox_pair{
+    /*
+        Class for convex function. Containing 
+            - function<Real(T)> h: the value of function. 
+            - function<T(T,Real)> proxh: the proximal operator of function. proxh(x,t) returns the scaled proximal operator
+    */
+
     public:
+
+    /* construction functions */
     prox_pair(){}
     prox_pair(function<Real(T)> h, function<T(T, Real)> proxh);
 
+    /* members */
     function<Real(T)> h;
     function<T(T,Real)> proxh;
 };
 
+
+
+/* -----------------------------
+        CLASS DEFINITION
+----------------------------  */
 
 template<typename T>
 grad_pair<T>:: grad_pair(function<Real(T)> f, function<T(T)> gradf){
@@ -41,7 +79,9 @@ prox_pair<T>:: prox_pair(function<Real(T)> h, function<T(T, Real)> proxh){
     this->h = h; this->proxh = proxh;
 }
 
-// some basic implementations.
+/*--------------------------------
+        BUILT-IN IMPLEMENTATIONS
+------------------------------- */
 
 
 Real LS_F(Mat A, Vec b, Vec x){return .5*(A*x-b).squaredNorm();}
@@ -53,4 +93,8 @@ Real L1_NORM_H(Vec x){return x.lpNorm<1>();}
 Vec L1_NORM_PROXH(Vec x, Real t){return (x.array().sign())*(x.array() - t).max(0);}
 prox_pair<Vec> L1_NORM(L1_NORM_H,L1_NORM_PROXH);
 
-#endif
+
+
+
+#endif    // FUNCPAIRS_HPP
+
