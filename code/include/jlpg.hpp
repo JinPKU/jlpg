4aeee42a091367e8b330be3e1c5fdc8e14506579
jinzy <jinzy@pku.edu.cn>
2021-01-06 13:52:10 +0800

    test

---
+++ code/include/jlpg.hpp
@@ -0,0 +1,15 @@
+#ifndef __JLPG_HPP__
+#define __JLPG_HPP__
+
+#ifndef __FUNCPAIRS_HPP__
+#include "funcpairs.hpp"
+#endif
+
+#ifndef __PROBLEM_HPP__
+#include "problem.hpp"
+#endif
+
+#ifndef __SOLVER_HPP__
+#include "solver.hpp"
+#endif 
+#endif  /* __JLPG_HPP__ */
