3160868d7c9b7b10ee38638bf9e60d0ec9f74d03
jinzy <jinzy@pku.edu.cn>
2021-01-12 21:04:31 +0800

    continuation and revise bugs

--- code/example/ex1.cpp
+++ code/example/ex1.cpp
@@ -7,21 +7,32 @@
 using namespace std;
 
 int main(){
-    Mat A = Mat::Random(500,1000);
-    Vec u = Vec::Random(1000);
+    Mat A = Mat::Random(256,512);
+    Vec u = Vec::Random(512);
     Vec u0 = 10*(u.array().sign())*(u.array().abs() - 0.9).max(0);
     Vec b = A*u0;
-    Vec x = Vec::Zero(1000);
+    Vec x = Vec::Zero(512);
     Problem<Vec> p(LS(A,b),L1_NORM);
-    p.mu = 10;
-    Options opts(10000, 1e-8, 1e-6, 1e-0, 5e-1);
+    Options opts(10000, 1e-10, 1e-8, 1e-1, 5e-1);
     opts.setClassical();
 
+    p.mu = 1e2;
     x = pgm(p,x,opts);
+
+    p.mu = 1e0;
+    x = pgm(p,x,opts);
+
+    p.mu = 1e-2;
+    x = pgm(p,x,opts);
+
+    p.mu = 1e-4;
+    x = pgm(p,x,opts);
+
+
     int cnt = 0;
-    for(int i = 0; i < 1000; i++){
+    for(int i = 0; i < 512; i++){
         if(abs(x[i])<1e-4) cnt ++ ;
     }
-    cout << cnt << endl;
+    cout << cnt / double(512) << endl;
     cout << (x-u0).lpNorm<1>()/u0.lpNorm<1>() << endl;
 }
