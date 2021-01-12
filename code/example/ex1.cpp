36903840191fa674c7245ee343d80ccd13e345f6
Ting Lin <lintingsms@pku.edu.cn>
2021-01-12 21:11:09 +0800

    Update ex1.cpp

--- code/example/ex1.cpp
+++ code/example/ex1.cpp
@@ -13,7 +13,6 @@ int main(){
     Vec b = A*u0;
     Vec x = Vec::Zero(512);
     Problem<Vec> p(LS(A,b),L1_NORM);
-<<<<<<< HEAD
     Options opts(10000, 1e-10, 1e-8, 1e-1, 5e-1);
     opts.setClassical();
 
@@ -36,16 +35,4 @@ int main(){
     }
     cout << cnt / double(512) << endl;
     cout << (x-u0).lpNorm<1>()/u0.lpNorm<1>() << endl;
-=======
-    p.mu = 0.01;
-    cout << p.value(u0) << endl;
-    p.mu*=(1<<15);
-    Options opts(10000, 1e-8*(1<<15), 1e-6*(1<<15), 1e-0, 5e-1);
-    opts.setClassical();
-
-    for(int i = 0; i < 15; i ++){
-        x = pgm(p,x,opts);
-        p.mu/=2, opts.ftol/=2,opts.gtol/=2;
-    }
->>>>>>> f2048ea3be15149c6cd4e01b4d7fc22bf04093b8
 }
