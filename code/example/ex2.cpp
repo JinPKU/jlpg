5ad8442afa5072ac524c378db83f69ddf6b22512
jinzy <jinzy@pku.edu.cn>
2021-01-12 23:14:30 +0800

    revise bugs

--- code/example/ex2.cpp
+++ code/example/ex2.cpp
@@ -12,15 +12,16 @@ int main(){
     cout << u << endl;
     Vec b = A*u;
     Vec x(3); x<<0,0,0;
-    Problem<Vec> p(LS(A,b),Linf_BALL(0.1));
-    p.mu = 0.01;
+    Problem<Vec> p(LS(A,b),L1_NORM);
+    p.mu = 0.0001;
     Options opts(10000, 1e-8, 1e-6, 1e-0, 5e-1);
     opts.setClassical();
-    ContOptions con_opts(opts, 100, 0.1, 100, 1e5, 1e5, 1e-1, 1e-1);
+    ContOptions con_opts(opts, 10, 0.1, 100, 1e2, 1e2, 1e-1, 1e-1);
 
     Outputs out;
     
     x = cont_pgm(p, x, con_opts, out);
+    // x = pgm(p, x, opts, out);
     cout << x << endl;
     cout << "cputime=" << out.cputime << endl;
     cout << "f=" << out.F_cur << "; nrmG=" << out.nrmG << "; Flag=" << out.Flag << endl;
