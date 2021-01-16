3496f6faba1e6503bdd7c2fe043d693376840fc1
jinzy <jinzy@pku.edu.cn>
2021-01-16 17:24:50 +0800

    group lasso

--- code/example/ex2.cpp
+++ code/example/ex2.cpp
@@ -1,27 +1,43 @@
-// constructing a lasso problem
+// Group Lasso problem
+
 #include <iostream>
 #include <eigen3/Eigen/Dense>
-#include "jlpg.hpp"
+#include "../include/jlpg.hpp"
 
 using namespace std;
 
 int main(){
-    Mat A = Mat::Random(3,3);
-    Vec u = Vec::Random(3);
-    cout << u << endl;
-    Vec b = A*u;
-    Vec x(3); x<<0,0,0;
-    Problem<Vec> p(LS(A,b),L1_NORM);
-    p.mu = 0.0001;
-    Options opts(10000, 1e-8, 1e-6, 1e-0, 5e-1);
-    opts.setClassical();
-    ContOptions con_opts(opts, 10, 0.1, 100, 1e2, 1e2, 1e-1, 1e-1);
-
-    Outputs out;
+    int m = 128, n = 256, l = 2;
+    Mat A = Mat::Random(m, n);
+    Mat u = Mat::Random(n, l);
+    Mat u0 = 10 * (u.array().sign()) * (u.array().abs() - 0.9).max(0);
+    Mat b = A * u0;
+    Mat x = Mat::Zero(n, l);
     
-    x = cont_pgm(p, x, con_opts, out);
-    // x = pgm(p, x, opts, out);
-    cout << x << endl;
-    cout << "cputime=" << out.cputime << endl;
-    cout << "f=" << out.F_cur << "; nrmG=" << out.nrmG << "; Flag=" << out.Flag << endl;
+    Problem<Mat> p(LS(A,b), L12_NORM);
+    p.mu = 1e-2;
+
+    Options opts(200, 1e-10, 1e-8, 1e-1, 5e-1);
+    ContOptions con_opts(opts, 100, 0.1, 100, 1e6, 1e6, 1e-1, 1e-1);
+    Outputs out;
+
+    Mat x1 = x, x2 = x, x3 = x;
+    printf("type & cont. & iters & cputime & fval & optimality & flag \\\\\n");
+    
+    // Armijo + continuation
+    opts.setArmijo(0.9);
+    x1 = cont_pgm(p, x1, con_opts, out);
+    printf("Armijo & 1 & %d & %g & %g & %g & %d \\\\\n", out.iter, out.cputime, out.F_cur, out.nrmG, out.Flag);
+
+    // Nonmonotone + continuation
+    opts.setNonmonotone(0.9, 5);
+    x2 = cont_pgm(p, x2, con_opts, out);
+    printf("Nonmonotone & 1 & %d & %g & %g & %g & %d \\\\\n", out.iter, out.cputime, out.F_cur, out.nrmG, out.Flag);
+
+    // Classical + continuation
+    opts.setClassical();
+    x3 = cont_pgm(p, x3, con_opts, out);
+    printf("Classical & 1 & %d & %g & %g & %g & %d \\\\\n", out.iter, out.cputime, out.F_cur, out.nrmG, out.Flag);
+
+    
 }
